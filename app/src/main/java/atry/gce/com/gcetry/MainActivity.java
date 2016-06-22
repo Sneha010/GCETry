670f4bb0cda93fe6bab28d0a638d5e111531f2ab
Sneha010 <sneha.khadatare@gmail.com>
2016-06-22 16:07:14 +0530

    Initial commit

---
+++ app/src/main/java/atry/gce/com/gcetry/MainActivity.java
@@ -0,0 +1,17 @@
+package atry.gce.com.gcetry;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+    }
+}
